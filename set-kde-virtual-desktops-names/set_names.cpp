9f71dfbb0d9f2279b5d542431bf997977863f531
Shlomi Fish <shlomif@iglu.org.il>
2011-06-15 19:41:40 +0300

    Add an opening comment.

--- set-kde-virtual-desktops-names/set_names.cpp
+++ set-kde-virtual-desktops-names/set_names.cpp
@@ -1,3 +1,36 @@
+/*
+ * A program to set the names of KDE 4' virtual desktops (a.k.a virtual
+ * workspaces).
+ *
+ * Adapted from kdebase-workspace-4.6.4 's 
+ * kwin/kcmkwin/kwindesktop/main.cpp , which is:
+********************************************************************
+KWin - the KDE window manager
+This file is part of the KDE project.
+
+Copyright (C) 2009 Martin Gräßlin <kde@martin-graesslin.com>
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*********************************************************************
+ *
+ * Skeleton of KApplication taken from:
+ * http://techbase.kde.org/Development/Tutorials/First_program
+ *
+ * Written by Shlomi Fish ( http://www.shlomifish.org/ ) while disclaiming
+ * all rights on the modifications.
+ *
+ */
 #include <QX11Info>
 #include <NETRootInfo>
 #include <KApplication>
